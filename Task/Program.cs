c6dce4f2658619585d00f48d8eb963cffe255032
Kate <cvetok_spb@bk.ru>
2022-11-23 19:21:50 +0300

    Added Task

---
+++ Task/Program.cs
@@ -0,0 +1,32 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше или равна 3 символа.
+
+string[] firstArray = { "hello", "2", "world", ":-)" };
+string[] secondArray = new string[firstArray.Length - 1];
+
+void CountSymbols(string[] firstArray, string[] secondArray)
+{
+    int count = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i].Length <= 3)
+        {
+            secondArray[count] = firstArray[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < array.Length - 1) Console.Write(array[i] + " ");
+        else Console.Write(array[i] + "]");
+    }
+    Console.WriteLine();
+}
+CountSymbols(firstArray, secondArray);
+Console.Write($"Исходный массив: ");
+PrintArray(firstArray);
+Console.Write($"Новый массив: ");
+PrintArray(secondArray);
